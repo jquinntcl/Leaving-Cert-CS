8bf05706151b48dd66a03e79391c979df8322ac6
jquinntcl <jquinn.tcl@lwetb.ie>
2025-11-09 10:50:05 +0000

    docs: Add printingHelloWorld.py example for LC-2025

---
+++ LC-2025/python/printingHelloWorld.py
@@ -0,0 +1,16 @@
+#Python - printing
+#28/8/24
+
+#Task1
+print("Hello World")
+
+#Task 2
+print("Question: What do you call it when your mom's mom is on social media? ")
+print("Answer: Instagram")
+
+#Task 3
+print("\U0001f600")
+
+#Task 4 - special characters
+print("Python\n is cool") #\n creates a new line
+print("*\tPython is cool\t*") #\t tab creates space
