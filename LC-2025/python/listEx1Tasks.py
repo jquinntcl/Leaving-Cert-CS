ae2eeddd031efd902f592178c729d115fc3148fe
jquinntcl <jquinn.tcl@lwetb.ie>
2025-11-09 11:09:25 +0000

    listEx1Tasks.py

---
+++ LC-2025/python/listEx1Tasks.py
@@ -0,0 +1,24 @@
+#Lists
+#Ex 1 Task 1
+print("The original list")
+numberList = [1,2,3,4,5,6,7,8,9,10]
+print(numberList)
+
+#Ex 1 Task 2
+numberList.append(11)
+print("Add 11 to the end of the list")
+print(numberList)
+
+numberList.insert(5,12)
+print("Inserting 12 at index position 5")
+print(numberList)
+
+numberList.remove(7)
+print("Removing 7 from the list")
+print(numberList)
+
+#Ex 1 task 3
+numberList = [1,2,3,4,5,6,7,8,9,10]
+for number in numberList:
+    print(number)
+
